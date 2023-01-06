77b1c7ffc40730cccc3ac3dfeb99ea04882dcb5c
flare-s <webflarez@gmail.com>
2023-01-06 08:50:25 +0000

    Show the state of archived tasks on UI

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -23,10 +23,32 @@
       title: '',
       state: '',
     };
+    $: isChecked = task.state === "TASK_ARCHIVED";
+
   </script>
   
   <div class="list-item">
+    <label for="checked" class="checkbox" aria-label={`archiveTask-${task.id}`}>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          disabled
+          name="checked"
+          id={`archiveTask-${task.id}`}
+        />
+        <span class="checkbox-custom" on:click={ArchiveTask} />
+      </label>
     <label for="title" aria-label={task.title}>
       <input type="text" value={task.title} name="title" readonly />
     </label>
+    {#if task.state !== "TASK_ARCHIVED"}
+    <button
+      class="pin-button"
+      on:click|preventDefault={PinTask}
+      id={`pinTask-${task.id}`}
+      aria-label={`pinTask-${task.id}`}
+    >
+      <span class="icon-star" />
+    </button>
+  {/if}
   </div>
